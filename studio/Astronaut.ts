188f7e101d8305b66ebe85109d40fbf95c9d4b51
rania <raniaa13.rh@gmail.com>
2021-08-05 20:58:59 -0500

    last method

---
+++ studio/Astronaut.ts
@@ -0,0 +1,9 @@
+export class Astronaut {
+    // properties and methods
+    massKg : number;
+    name : string;
+    constructor(massKg: number, name : string) {
+        this.massKg = massKg;
+        this.name = name;
+     }
+ }
---
+++ studio/Cargo.ts
@@ -0,0 +1,9 @@
+export class Cargo {
+    // properties and methods
+    massKg : number;
+    material  : string;
+    constructor(massKg: number, material : string) {
+        this.massKg = massKg;
+        this.material = material;
+ }
+}
